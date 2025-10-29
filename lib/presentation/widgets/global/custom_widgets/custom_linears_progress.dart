a61f2703b163401e58ce48a71eb5b0a141004e83
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2025-10-29 21:30:52 +0200

    Add files via upload

---
+++ lib/presentation/widgets/global/custom_widgets/custom_linears_progress.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThineLinearProgress extends StatelessWidget {
+  const ThineLinearProgress({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(minHeight: 1, color: Theme.of(context).focusColor);
+  }
+}
